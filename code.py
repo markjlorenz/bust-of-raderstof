cd36f9e5902c4766ccf0c0953bf57fb7bc2a2f31
Mark Lorenz <mlorenz@andhealth.com>
2022-07-07 00:19:48 -0400

    add 3d models, and add a battery gauge

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-import time, alarm
+import time, alarm, math
 import board, digitalio, touchio
 import feathers3
 
@@ -16,6 +16,25 @@ def startIdeaTime():
 
 def stopIdeaTime():
     led_1.value=False
+
+# LiPo is good from 3.2 to 4.2 volts, so we will blink a reverse-percentage
+# 0 blinks means we are fully charged.
+# 10 blinks means the battery is at 3.2v and must be charged immediatly.
+# 5 blinks means the battery is at 3.7v mid-charge.
+#
+def batteryReport():
+    voltage = feathers3.get_battery_voltage()
+    print("Battery: {:0.1f}\n".format(voltage))
+    BATTERY_MIN = 3.2
+    BATTERY_MAX = 4.2
+    batteryPercent = (voltage - BATTERY_MIN) / (BATTERY_MAX - BATTERY_MIN)
+    batteryBlinks = math.floor(10 - batteryPercent * 10)
+    while batteryBlinks > 0:
+        feathers3.led_set(True)
+        time.sleep(0.1)
+        batteryBlinks -= 1
+        feathers3.led_set(False)
+        time.sleep(0.1)
 
 wakeAlarm = alarm.touch.TouchAlarm(board.D18)
 #wakeAlarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 15)
@@ -46,11 +65,10 @@ while True:
         print("Nap time")
         stopIdeaTime()
         touchLatch.deinit()
+        batteryReport()
         alarm.light_sleep_until_alarms(wakeAlarm)
 
         print("Idea time!")
         startIdeaTime()
         touchLatch = initTouchLatch()
         wakeUntil = time.monotonic() + MINIMUM_TIME
-
-
